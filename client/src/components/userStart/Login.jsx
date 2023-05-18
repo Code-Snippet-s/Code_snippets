3df6abab31958009e050ae29ce8caf63316570cd
Kyle Slugg <kyle.slugg@gmail.com>
2023-05-18 16:49:21 -0400

    Resolved merge conflicts

--- client/src/components/userStart/Login.jsx
+++ client/src/components/userStart/Login.jsx
@@ -4,54 +4,46 @@ import styles from './Login.module.scss';
 
 // eslint-disable-next-line react/prop-types
 const Login = ({ handleLogin, handleHaveAccount, style, error }) => {
-
   return (
-    <div className='login'>
-      <div id='loginBox'>
+    <div className="login">
+      <div id="loginBox">
         <h1>USER LOGIN</h1>
-        <form action='/action_page.php' className='form'>
-          <div className='form-group'>
-            <label htmlFor='username '>USERNAME:</label>&nbsp; &nbsp;
+        <form action="/action_page.php" className="form">
+          <div className="form-group">
+            <label htmlFor="username ">USERNAME:</label>&nbsp; &nbsp;
             <input
-              id='username'
-
+              id="username"
               className={`form-group ${styles[style]}`}
-
-              type='text'
-              placeholder='username...'
+              type="text"
+              placeholder="username..."
             />
           </div>
-          <div className='form-group' id='secondDiv'>
-            <label htmlFor='password'>PASSWORD:</label>&nbsp; &nbsp;
+          <div className="form-group" id="secondDiv">
+            <label htmlFor="password">PASSWORD:</label>&nbsp; &nbsp;
             <input
-              id='password'
-
+              id="password"
               className={`form-group ${styles[style]}`}
-
-              type='password'
-              placeholder='password...'
+              type="password"
+              placeholder="password..."
             />
           </div>
           <button
             onClick={handleLogin}
-            type='submit'
-            className='btn btn-default'
-
-            id='go'>
+            type="submit"
+            className="btn btn-default"
+            id="go"
+          >
             GO
           </button>
         </form>
-         //<p className='errorMessage'></p>
-        {error && <p className={styles.p}>wrong username or password!</p>}
-
+        <p className="errorMessage"></p>
       </div>
       <button
-        id='bottomMessage'
+        id="bottomMessage"
         onClick={handleHaveAccount}
-        type='submit'
-
-        className='btn btn-default'>
-
+        type="submit"
+        className="btn btn-default"
+      >
         Click Me to Create Your Account!
       </button>
     </div>
